bb190fcc2e7d05a699a03a53b38ec3b2e3ddcfa0
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-10-15 17:23:22 +0300

    complete

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,18 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface PropsPage {
+	title: 'string';
+}
 
-class Component {
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<PropsPage> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
